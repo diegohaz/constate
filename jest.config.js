78cb0bbe2181dc1079a9d018546107ae767b4c25
Haz <hazdiego@gmail.com>
2018-08-21 22:40:19 -0300

    Fix tests

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,9 @@ const { defaults } = require("jest-config");
 module.exports = {
   snapshotSerializers: ["enzyme-to-json/serializer", "jest-serializer-html"],
   coveragePathIgnorePatterns: ["/node_modules/", "<rootDir>/test/"],
-  setupFiles: ["<rootDir>/test/testSetup.js"],
+  setupFiles: ["<rootDir>/test/testSetup.ts"],
   moduleFileExtensions: [...defaults.moduleFileExtensions, "ts", "tsx"],
+  testMatch: ["**/?(*.)+(spec|test).(j|t)s?(x)"],
   transform: {
     // (.js, .ts, .jsx, .tsx) files
     "\\.(j|t)sx?$": "babel-jest"
