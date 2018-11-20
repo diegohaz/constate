29aa9f33834cc533e321b0d5f3f04bb75fa69fb6
Haz <hazdiego@gmail.com>
2018-11-20 15:35:18 -0200

    Finish examples setup

--- examples/counter/index.js
+++ examples/counter/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Counter from "./counter/App";
-
-const App = () => <Counter />;
+import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
