6608247ed10a6edb641f9bfb9a7c60c2e76fcb91
Haz <hazdiego@gmail.com>
2019-01-28 17:12:37 -0200

    chore: Upgrade dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   collectCoverageFrom: ["src/**/*.{ts,tsx,js}", "!src/utils/useDevtools.ts"],
   coveragePathIgnorePatterns: ["/node_modules/", "<rootDir>/test/"],
-  setupTestFrameworkScriptFile: "<rootDir>/jest.setup.js",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
   testMatch: ["**/?(*.)+(spec|test).(j|t)s?(x)"],
   transform: {
