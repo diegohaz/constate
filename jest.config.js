e02fb9463ef63f03ccec72d626e59ea72f90b31e
Haz <hazdiego@gmail.com>
2019-01-28 18:01:52 -0200

    chore: Remove useless jest configuration for typescript

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   collectCoverageFrom: ["src/**/*.{ts,tsx,js}", "!src/utils/useDevtools.ts"],
   coveragePathIgnorePatterns: ["/node_modules/", "<rootDir>/test/"],
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
-  testMatch: ["**/?(*.)+(spec|test).(j|t)s?(x)"],
-  transform: {
-    "\\.(j|t)sx?$": "babel-jest"
-  }
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"]
 };
