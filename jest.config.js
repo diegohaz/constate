5ad498a50f73eae6c4c374e1c5dda9907db5b1a7
Haz <hazdiego@gmail.com>
2019-10-10 17:05:19 -0600

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  collectCoverageFrom: ["src/**/*.{ts,tsx,js}", "!src/utils/useDevtools.ts"],
   coveragePathIgnorePatterns: ["/node_modules/", "<rootDir>/test/"],
   setupFilesAfterEnv: ["raf/polyfill"]
 };
